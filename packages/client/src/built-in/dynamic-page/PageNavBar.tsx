bb80db2a06e8fda8650233d82f3e4d6008ba422b
bai.zixv <bai.zixv@foxmail.com>
2025-07-16 11:58:10 +0800

    feat: page style

--- packages/client/src/built-in/dynamic-page/PageNavBar.tsx
+++ packages/client/src/built-in/dynamic-page/PageNavBar.tsx
@@ -7,6 +7,8 @@ import { useNavigate } from 'react-router-dom';
 import { useRequest } from '../../api-client';
 import { useCompile } from '../../schema-component';
 import { useDocumentTitle } from '../document-title';
+import { PageStyle } from '../page-style/PageStyle.provider';
+import { usePageStyle } from '../page-style/usePageStyle';
 import { useStyles } from './PageNavBar.style';
 
 const { Title } = Typography;
@@ -27,10 +29,17 @@ export const PageNavBar = ({ uid }: { uid: string }) => {
     return compile(currentTitle || documentTitle);
   }, [currentTitle, documentTitle]);
 
+  const pageStyle = usePageStyle();
+
   // 处理返回操作
   const handleBack = () => {
     navigate(-1);
   };
+
+  // 页面模式为多标签页时, 不显示导航栏, 标签本身支持返回功能
+  if (pageStyle === PageStyle.TAB_STYLE) {
+    return null;
+  }
 
   return (
     <div className={styles['page-nav-bar']}>

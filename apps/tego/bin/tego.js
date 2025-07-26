734b3ebd348a329d4bed28978ce1092bb33d63fb
sealday <sealday@gmail.com>
2025-07-27 00:02:55 +0800

    fix: version mismatch

--- apps/tego/bin/tego.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-const chalk = require('chalk');
-
-if (__dirname.startsWith('/snapshot/')) {
-  console.log(chalk.green('WAIT: ') + 'Engine is loading...');
-}
-
-require('../lib/index.js');

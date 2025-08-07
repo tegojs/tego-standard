e0aa9a598c5f3ca530f3c4cd37c43a948a5654fa
bai.zixv <602250248@qq.com>
2025-08-07 16:38:42 +0800

    feat: close tab logic

--- packages/client/src/built-in/page-style/useCloseTab.ts
+++ packages/client/src/built-in/page-style/useCloseTab.ts
@@ -3,6 +3,7 @@ import { useCallback, useContext } from 'react';
 import { useNavigate } from 'react-router';
 import { useLocation } from 'react-router-dom';
 
+import { useIsSubPage } from '../../application/CustomRouterContextProvider';
 import { PageStyleContext } from './PageStyle.provider';
 
 export const useCloseTab = () => {
@@ -11,6 +12,9 @@ export const useCloseTab = () => {
   const { items, setItems } = useContext(PageStyleContext);
   const targetKey = location.pathname;
 
+  const isSubPage = useIsSubPage();
+
+  // 处理关闭标签页后的逻辑，包括跳转、兜底等
   const handleCloseTab = useCallback(
     (e, item) => {
       e?.stopPropagation();
@@ -25,13 +29,18 @@ export const useCloseTab = () => {
             // 没有标签页了，跳转到首页
             navigate('/');
           } else {
-            // 优先跳转到右侧标签页，否则跳转到左侧
-            const nextTab = items[idx + 1] || items[idx - 1];
-            if (nextTab) {
-              navigate(nextTab.key);
+            // 如果当前页面是子页面,优先返回上一页
+            if (isSubPage && window?.history.length > 1) {
+              navigate(-1);
             } else {
-              // 理论上不会走到这里，但兜底跳转首页
-              navigate('/');
+              // 优先跳转到右侧标签页，否则跳转到左侧
+              const nextTab = items[idx + 1] || items[idx - 1];
+              if (nextTab) {
+                navigate(nextTab.key);
+              } else {
+                // 理论上不会走到这里，但兜底跳转首页
+                navigate('/');
+              }
             }
           }
         }

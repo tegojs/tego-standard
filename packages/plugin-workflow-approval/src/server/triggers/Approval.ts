fd2780b87bd6aa7925260643ea4b22bbfa94ddc7
bai.zixv <602250248@qq.com>
2025-10-17 18:33:46 +0800

    fix: workflow approval, old version support withdraw (#211)
    * fix: approval
    
    * fix: workflow approval, old version support withdraw

--- packages/plugin-workflow-approval/src/server/triggers/Approval.ts
+++ packages/plugin-workflow-approval/src/server/triggers/Approval.ts
@@ -1,7 +1,7 @@
 import { UiSchemaRepository } from '@tachybase/module-ui-schema';
 import { EXECUTION_STATUS, JOB_STATUS, toJSON, Trigger } from '@tachybase/module-workflow';
-
 import { modelAssociationByKey, parseCollectionName } from '@tego/server';
+
 import { get } from 'lodash';
 import { BelongsTo, HasOne, Op } from 'sequelize';
 

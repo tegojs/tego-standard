d56369e6897bd5ef09ff681b957abc7808c104be
TomyJan <TomyJan6@gmail.com>
2025-10-20 17:40:59 +0800

    perf: extra translation

--- packages/module-user/src/server/locale/zh-CN.ts
+++ packages/module-user/src/server/locale/zh-CN.ts
@@ -5,6 +5,4 @@ export default {
   'Not a valid cellphone number, please re-enter': '不是有效的手机号，请重新输入',
   'The phone number has been registered, please login directly': '手机号已注册，请直接登录',
   'The phone number is not registered, please register first': '手机号未注册，请先注册',
-  'User status does not allow login': '用户状态不允许登录',
-  'Invalid user status': '无效的用户状态',
 };

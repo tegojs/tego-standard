80e63e34566b9e81c2a789176d9aa05b0f92a3d3
TomyJan <TomyJan6@gmail.com>
2025-10-24 13:44:46 +0800

    fix: build

--- packages/plugin-field-bank-card-number/src/client/locale.ts
+++ packages/plugin-field-bank-card-number/src/client/locale.ts
@@ -1,11 +1,4 @@
-import { useTranslation } from 'react-i18next';
-
 export const NAMESPACE = 'field-bank-card-number';
-
-export function useLang(key: string, options = {}) {
-  const { t } = useTranslation(NAMESPACE, options);
-  return t(key);
-}
 
 export function tval(key: string) {
   return `{{t('${key}', { ns: '${NAMESPACE}', nsMode: 'fallback' })}}`;

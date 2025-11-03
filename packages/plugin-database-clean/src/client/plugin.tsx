e918442ef16f417f17e444ef2a4f627ea602064c
bai.zixv <602250248@qq.com>
2025-11-03 12:15:39 +0800

    chore: create new plugin, plugin-database-clean

---
+++ packages/plugin-database-clean/src/client/plugin.tsx
@@ -0,0 +1,11 @@
+import { Plugin } from '@tego/client';
+
+class PluginPluginDatabaseClean extends Plugin {
+  async afterAdd() {}
+
+  async beforeLoad() {}
+
+  async load() {}
+}
+
+export default PluginPluginDatabaseClean;
---
+++ packages/plugin-database-clean/src/server/plugin.ts
@@ -0,0 +1,19 @@
+import { Plugin } from '@tego/server';
+
+export class PluginPluginDatabaseClean extends Plugin {
+  async afterAdd() {}
+
+  async beforeLoad() {}
+
+  async load() {}
+
+  async install() {}
+
+  async afterEnable() {}
+
+  async afterDisable() {}
+
+  async remove() {}
+}
+
+export default PluginPluginDatabaseClean;

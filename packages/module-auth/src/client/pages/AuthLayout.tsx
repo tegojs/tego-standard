493fda2fe0a6e9e94a1815a326439438a666dcad
TomyJan <TomyJan6@gmail.com>
2025-11-28 14:59:52 +0800

    test: remove debug logging from AuthLayout

--- packages/module-auth/src/client/pages/AuthLayout.tsx
+++ packages/module-auth/src/client/pages/AuthLayout.tsx
@@ -9,48 +9,12 @@ export function AuthLayout(props: any) {
   const { data } = useSystemSettings();
   const api = useAPIClient();
 
-  console.log('[AuthLayout] Component rendering, about to call useRequest');
-
-  const { data: authenticators = [], error } = useRequest(() => {
-    console.log('[AuthLayout] useRequest factory function called');
-    return api
+  const { data: authenticators = [], error } = useRequest(() =>
+    api
       .resource('authenticators')
       .publicList()
-      .then((res) => {
-        console.log('[AuthLayout] API response received:', {
-          fullResponse: res,
-          hasRes: !!res,
-          resType: typeof res,
-          resKeys: res ? Object.keys(res) : null,
-          hasData: !!res?.data,
-          dataType: typeof res?.data,
-          dataKeys: res?.data ? Object.keys(res.data) : null,
-          dataValue: res?.data,
-          hasDataData: !!res?.data?.data,
-          dataDataType: typeof res?.data?.data,
-          dataDataIsArray: Array.isArray(res?.data?.data),
-          dataDataValue: res?.data?.data,
-        });
-
-        // api.resource().publicList() already unwraps once: returns { data: [...] }
-        // So res.data is the array directly
-        const result = res?.data || [];
-        console.log('[AuthLayout] Returning from then():', {
-          resultType: typeof result,
-          resultIsArray: Array.isArray(result),
-          resultValue: result,
-        });
-        return result;
-      });
-  });
-
-  console.log('[AuthLayout] After useRequest:', {
-    authenticatorsType: typeof authenticators,
-    authenticatorsIsArray: Array.isArray(authenticators),
-    authenticatorsLength: Array.isArray(authenticators) ? authenticators.length : 'N/A',
-    authenticatorsKeys: authenticators && !Array.isArray(authenticators) ? Object.keys(authenticators) : null,
-    authenticatorsValue: authenticators,
-  });
+      .then((res) => res?.data?.data || []),
+  );
 
   if (error) {
     throw error;

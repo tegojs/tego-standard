ddeba5baf5fb4e21ce1a6134016be7e12270d9ef
bai.zixv <602250248@qq.com>
2026-04-18 10:50:32 +0800

    feat: add resource trigger options

--- packages/module-event-source/src/client/triggers/CustomActionTrigger.ts
+++ packages/module-event-source/src/client/triggers/CustomActionTrigger.ts
@@ -1,88 +1,9 @@
 import { EventSourceTrigger } from '.';
 import { NAMESPACE, tval } from '../locale';
+import { createResourceTriggerOptions } from './resourceTriggerOptions';
 
 export class CustomActionTrigger extends EventSourceTrigger {
   title = tval('Custom resource action');
   description = tval('for creating custom requests, try not to duplicate with other requests');
-  options = {
-    resourceName: {
-      type: 'string',
-      title: tval('Resource name'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Input',
-    },
-    actionName: {
-      type: 'string',
-      title: tval('Action name'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Input',
-    },
-    triggerOnAssociation: {
-      type: 'boolean',
-      title: tval('Trigger on association'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Checkbox',
-    },
-    useHttpContext: {
-      type: 'boolean',
-      title: tval('Pass HTTP context to workflow'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Checkbox',
-    },
-    failurePolicy: {
-      type: 'string',
-      title: tval('Failure policy'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Select',
-      enum: [
-        { label: tval('Ignore failure'), value: 'ignore' },
-        { label: tval('Block request on failure'), value: 'block' },
-      ],
-      default: 'ignore',
-    },
-    timeoutMs: {
-      type: 'number',
-      title: tval('Timeout (ms)'),
-      'x-decorator': 'FormItem',
-      'x-component': 'NumberPicker',
-      'x-component-props': {
-        min: 0,
-        step: 100,
-      },
-      default: 0,
-    },
-    executionMode: {
-      type: 'string',
-      title: tval('Execution mode'),
-      'x-decorator': 'FormItem',
-      'x-component': 'Select',
-      enum: [
-        { label: tval('Inline'), value: 'inline' },
-        { label: tval('Queue'), value: 'queue' },
-      ],
-      default: 'inline',
-    },
-    maxAttempts: {
-      type: 'number',
-      title: tval('Max attempts'),
-      'x-decorator': 'FormItem',
-      'x-component': 'NumberPicker',
-      'x-component-props': {
-        min: 1,
-        step: 1,
-      },
-      default: 3,
-    },
-    retryBackoffMs: {
-      type: 'number',
-      title: tval('Retry backoff (ms)'),
-      'x-decorator': 'FormItem',
-      'x-component': 'NumberPicker',
-      'x-component-props': {
-        min: 100,
-        step: 100,
-      },
-      default: 3000,
-    },
-  };
+  options = createResourceTriggerOptions();
 }

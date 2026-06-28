cabaa93a440ea0a37368572afd0cf88caeef6e78
TomyJan <TomyJan6@gmail.com>
2026-06-29 01:07:25 +0800

    Revert "test(工作流): 缩短定时测试等待时间"
    This reverts commit f5259b8fe9964c0d7b2fe23f0ebfa9a48da0099a.

--- packages/module-backup/src/server/__tests__/dumper.test.ts
+++ packages/module-backup/src/server/__tests__/dumper.test.ts
@@ -42,7 +42,6 @@ describe('dumper', () => {
     'should dump collection table structure',
     'should get dumped collections with origin option',
     'should get custom collections group',
-    'should dump collection meta',
   ]);
   let reusableBaseApp: MockServer | undefined;
   let shouldDestroyApp = false;
--- packages/module-workflow/src/server/__tests__/triggers/schedule/mode-date-field.test.ts
+++ packages/module-workflow/src/server/__tests__/triggers/schedule/mode-date-field.test.ts
@@ -18,8 +18,10 @@ function getRecordTriggerTime(record) {
   return triggerTime.getTime();
 }
 
-function getFutureSecond(minDelay = 800) {
-  return new Date(Math.ceil((Date.now() + minDelay) / 1000) * 1000);
+function getFutureSecond(seconds = 2) {
+  const date = new Date(Date.now() + seconds * 1000);
+  date.setMilliseconds(0);
+  return date;
 }
 
 async function sleepToEvenSecond() {
